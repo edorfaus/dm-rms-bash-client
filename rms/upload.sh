764a5198c80355143cc7ac7efea8f94d21cb7435
Frode Austvik <frode@documaster.no>
2017-01-16 15:10:04 +0100

    Improved the rms_upload function to support most filenames.
    This implements RFC5987 encoding of the filename and uploads as a raw
    file instead of using cURL's form support. This removes the previous
    limitations on the filename, but adds a new limitation of needing its
    charset to be in UTF-8 or being convertable to that with iconv.
    
    The old method is still available via a new parameter, to have the
    widest possible support. (Also useful for debugging the RMS.)

--- rms/upload.sh
+++ rms/upload.sh
@@ -2,11 +2,61 @@
 # This file provides a function to upload a document to the RMS.
 #
 rms_upload() {
-	local file="$1"
+	local file="$1" use_form=0
+	if [ "$file" = "--form" -a $# -gt 1 ]; then
+		# The user wants us to use the cURL form upload support
+		# instead of direct upload with manual name encoding.
+		# This is more limited in some ways, less in others.
+		use_form=1
+		file="$2"
+	fi
 
 	if [ "$file" = "" ]; then
 		echo "Error: The filename is required." >&2
 		return 1
+	fi
+
+	# Don't include the path in the filename we send
+	local basename="${file##*/}"
+
+	# Keep these for later, since we must set them temporarily
+	local ORIG_LANG="$LANG" ORIG_LC_ALL="$LC_ALL"
+
+	if [ $use_form -eq 0 ]; then
+		# Ensure that we encode the name to the correct charset
+		if [ "${LANG##*.}" != "UTF-8" ]; then
+			if ! basename=$(iconv -t "UTF-8" <<<"$basename")
+			then
+				echo "Error: Failed to convert charset for filename encoding." >&2
+				return 1
+			fi
+		fi
+
+		# Encode the filename as per RFC5987
+		local converted="UTF-8''"
+		local LANG=C LC_ALL=C
+		# Getting the length must be done while in the C locale
+		local len=${#basename} pos=0 hex char
+		while [ $pos -lt $len ]; do
+			char="${basename:$pos:1}"
+			if [[ "$char" =~ [a-zA-Z0-9!#$\&+.^_\`|~-] ]]; then
+				# Safe character, add as-is
+				converted="$converted$char"
+			else
+				# Non-safe character, percent-encode
+				hex=$(printf "%02x" "'${file:$pos:1}")
+				converted="$converted%$hex"
+			fi
+			pos=$(($pos + 1))
+		done
+		LANG="$ORIG_LANG"
+		LC_ALL="$ORIG_LC_ALL"
+		local header="Content-Disposition: attachment; filename*=$converted"
+
+		_rms_curl --type-octets --post-stdin /dots/v1/upload \
+			-- --header "$header" < "$file"
+
+		return $?
 	fi
 
 	if [[ "$file" == *'"'* ]]; then
@@ -14,8 +64,6 @@ rms_upload() {
 		return 1
 	fi
 
-	local ORIG_LANG="$LANG" ORIG_LC_ALL="$LC_ALL"
-	local basename="${file##*/}"
 	local LANG=C LC_ALL=C
 	if [[ "$basename" =~ [^$'\x20'-$'\x7e']|[%?/\\] ]]; then
 		echo "Warning: Filename contains unsupported character(s) that might get mangled." >&2
